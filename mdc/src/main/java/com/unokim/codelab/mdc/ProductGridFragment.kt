42a16ae124eaab78f4d6f4b21464153e77f0bc53
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-02 23:46:12 +0900

    MDC-104 Android: Material Advanced Components (Kotlin) https://codelabs.developers.google.com/codelabs/mdc-104-kotlin/index.html?index=..%2F..index#6 The completed MDC 104 app git checkout 104-complete

--- mdc/src/main/java/com/unokim/codelab/mdc/ProductGridFragment.kt
+++ mdc/src/main/java/com/unokim/codelab/mdc/ProductGridFragment.kt
@@ -1,8 +1,12 @@
 package com.unokim.codelab.mdc
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +23,7 @@ class ProductGridFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
+    @SuppressLint("ObsoleteSdkInt")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -29,6 +34,21 @@ class ProductGridFragment : Fragment() {
 
         // Set up the toolbar.
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        view.app_bar.setNavigationOnClickListener(
+            NavigationIconClickListener(
+                requireActivity(),
+                view.product_grid,
+                AccelerateDecelerateInterpolator(),
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.shr_branded_menu
+                ), // Menu open icon
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.shr_close_menu
+                ) // Menu close icon
+            )
+        )
 
         // Set up the RecyclerView
         view.recycler_view.setHasFixedSize(true)
@@ -41,6 +61,12 @@ class ProductGridFragment : Fragment() {
         val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
         val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
         view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.product_grid.background =
+                context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
 
         return view
     }
