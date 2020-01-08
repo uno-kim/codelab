fd65faf11248180d573a5727e0fb2f65b1451bb9
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-08 22:17:57 +0900

    Android Kotlin Fundamentals 01.3: Image resources and compatibility https://codelabs.developers.google.com/codelabs/kotlin-android-training-images-compat/#6 Solution code

--- diceroller/src/main/java/com/unokim/codelab/diceroller/MainActivity.kt
+++ diceroller/src/main/java/com/unokim/codelab/diceroller/MainActivity.kt
@@ -17,9 +17,8 @@
 package com.unokim.codelab.diceroller
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 /**
@@ -33,21 +32,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get the Button view from the layout and assign a click
-        // listener to it.
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        roll_button.setOnClickListener { rollDice() }
     }
 
     /**
      * Click listener for the Roll button.
      */
     private fun rollDice() {
-        // Toast.makeText(this, "button clicked",
-        //  Toast.LENGTH_SHORT).show()
-        val randomInt = Random().nextInt(6) + 1
-
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)
     }
 }
