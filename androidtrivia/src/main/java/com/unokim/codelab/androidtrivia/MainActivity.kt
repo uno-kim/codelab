55cf2640da72f65f36837373be6f13661e61d251
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-09 09:49:48 +0900

    Android Kotlin Fundamentals 03.1: Create a fragment https://codelabs.developers.google.com/codelabs/kotlin-android-training-create-and-add-fragment/index.html?index=..%2F..android-kotlin-fundamentals#4 Solution code

---
+++ androidtrivia/src/main/java/com/unokim/codelab/androidtrivia/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.unokim.codelab.androidtrivia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.unokim.codelab.androidtrivia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    // TODO (01) Create the new TitleFragment
+    // Select File->New->Fragment->Fragment (Blank)
+
+    // TODO (02) Clean up the new TitleFragment
+    // In our new TitleFragment
+
+    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
+    // In our new TitleFragment
+    // R.layout.fragment_title
+}
---
+++ androidtrivia/src/main/java/com/unokim/codelab/androidtrivia/TitleFragment.kt
@@ -0,0 +1,29 @@
+package com.unokim.codelab.androidtrivia
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.unokim.codelab.androidtrivia.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
