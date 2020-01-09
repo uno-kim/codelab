349a12a1a2b5cf2478c34a1f6bb8df736bf06328
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-09 22:43:03 +0900

    Android Kotlin Fundamentals 02.3: Constraint layout using the Layout Editor https://codelabs.developers.google.com/codelabs/kotlin-android-training-constraint-layout/index.html#13 Solution code

---
+++ colormyviews/src/main/java/com/unokim/codelab/colormyviews/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.unokim.codelab.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners() {
+        listOf(
+            box_one_text,
+            box_two_text,
+            box_three_text,
+            box_four_text,
+            box_five_text,
+            constraint_layout
+        ).forEach { view -> view.setOnClickListener { makeColored(it) } }
+    }
+}
