bd645231ad3545c01d3509687a4ea86f55551acf
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-10 13:40:34 +0900

    Android Kotlin Fundamentals 06.1: Create a Room database https://codelabs.developers.google.com/codelabs/kotlin-android-training-room-database/#6 Solution code

--- trackmysleepquality/src/main/java/com/unokim/codelab/trackmysleepquality/database/SleepDatabase.kt
+++ trackmysleepquality/src/main/java/com/unokim/codelab/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,36 @@
  */
 
 package com.unokim.codelab.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also {
+                    INSTANCE = it
+                }
+            }
+
+        private fun buildDatabase(context: Context): SleepDatabase =
+            Room.databaseBuilder(
+                context.applicationContext,
+                SleepDatabase::class.java,
+                "sleep_history_database"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+    }
+}
