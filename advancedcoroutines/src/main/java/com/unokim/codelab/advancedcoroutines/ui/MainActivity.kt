ebf71a04a81501ef7e4cfe3ed1206a2b421e9a2e
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-13 16:18:15 +0900

    Advanced Coroutines with Kotlin Flow and LiveData https://codelabs.developers.google.com/codelabs/advanced-kotlin-coroutines/index.html?index=..%2F..index#1 Starter code

---
+++ advancedcoroutines/src/main/java/com/unokim/codelab/advancedcoroutines/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.unokim.codelab.advancedcoroutines.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unokim.codelab.advancedcoroutines.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ advancedcoroutines/src/main/java/com/unokim/codelab/advancedcoroutines/utils/Injector.kt
@@ -0,0 +1,51 @@
+package com.unokim.codelab.advancedcoroutines.utils
+
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import com.unokim.codelab.advancedcoroutines.NetworkService
+import com.unokim.codelab.advancedcoroutines.PlantRepository
+import com.unokim.codelab.advancedcoroutines.ui.PlantListViewModelFactory
+
+interface ViewModelFactoryProvider {
+    fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory
+}
+
+val Injector: ViewModelFactoryProvider
+    get() = currentInjector
+
+private object DefaultViewModelProvider : ViewModelFactoryProvider {
+    private fun getPlantRepository(context: Context): PlantRepository {
+        return PlantRepository.getInstance(
+            plantDao(context),
+            plantService()
+        )
+    }
+
+    private fun plantService() = NetworkService()
+
+    private fun plantDao(context: Context) =
+        AppDatabase.getInstance(context.applicationContext).plantDao()
+
+    override fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory {
+        val repository = getPlantRepository(context)
+        return PlantListViewModelFactory(repository)
+    }
+}
+
+private object Lock
+
+@Volatile
+private var currentInjector: ViewModelFactoryProvider =
+    DefaultViewModelProvider
+
+
+@VisibleForTesting
+private fun setInjectorForTesting(injector: ViewModelFactoryProvider?) {
+    synchronized(Lock) {
+        currentInjector = injector ?: DefaultViewModelProvider
+    }
+}
+
+@VisibleForTesting
+private fun resetInjector() =
+    setInjectorForTesting(null)
