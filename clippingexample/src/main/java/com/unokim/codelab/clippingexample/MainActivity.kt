90bedd19acb8d551c32edc179860fed2302e0051
Min Sung Kim <ms.uno.kim@gmail.com>
2020-01-15 12:04:01 +0900

    Advanced Android in Kotlin 02.3: Clipping Canvas Objects https://codelabs.developers.google.com/codelabs/advanced-android-kotlin-training-clipping-canvas-objects/index.html?index=..%2F..advanced-android-kotlin-training#7 git clone https://github.com/googlecodelabs/android-kotlin-drawing-clipping Solution code

---
+++ clippingexample/src/main/java/com/unokim/codelab/clippingexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.unokim.codelab.clippingexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView(this))
+    }
+}
